8ac8527245f47920d04804871563b6303611a2ed
HaileeKim <0206dbsh@gmail.com>
2023-01-17 22:38:52 +0900

    add R-TOD to demo.c

--- src/detector.c
+++ src/detector.c
@@ -2104,24 +2104,13 @@ void run_detector(int argc, char **argv)
         if (filename)
             if (strlen(filename) > 0)
                 if (filename[strlen(filename) - 1] == 0x0d) filename[strlen(filename) - 1] = 0;
-        demo(cfg, weights, thresh, hier_thresh, cam_index, filename, names, classes, avgframes, frame_skip, prefix, out_filename,
-            mjpeg_port, dontdraw_bbox, json_port, dont_show, ext_output, letter_box, time_limit_sec, http_post_host, benchmark, benchmark_layers);
+        demo(datacfg, cfg, weights, thresh, hier_thresh, cam_index, filename, names, classes, avgframes, frame_skip, prefix, out_filename,
+            mjpeg_port, dontdraw_bbox, json_port, dont_show, ext_output, letter_box, time_limit_sec, http_post_host, benchmark, benchmark_layers, w, h, fps);
 
         free_list_contents_kvp(options);
         free_list(options);
     }
     
-    else if (0 == strcmp(argv[2], "rtod")) {
-        list *options = read_data_cfg(datacfg);
-        if (filename)
-            if (strlen(filename) > 0)
-                if (filename[strlen(filename) - 1] == 0x0d) filename[strlen(filename) - 1] = 0;
-        rtod(datacfg, cfg, weights, thresh, hier_thresh, cam_index, filename, frame_skip, prefix, out_filename,
-                mjpeg_port, json_port, dont_show, ext_output, letter_box, time_limit_sec, http_post_host, benchmark, benchmark_layers, w, h, fps);
-
-        free_list_contents_kvp(options);
-        free_list(options);
-    }
     else printf(" There isn't such command: %s", argv[2]);
 
     if (gpus && gpu_list && ngpus > 1) free(gpus);
